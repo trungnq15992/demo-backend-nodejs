e828b059b1c8f15eda034504a56ec61ea08191d1
TrungNQ <trungnq15992@gmail.com>
2022-12-21 15:48:51 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const configViewEngine = require('./config/viewEngine')
+const express = require('express');
+const configViewEngine = require('./config/viewEngine');
 const webRouters = require('./routes/web');
+const connection = require('./config/database');
 
-const app = express()
-const port = process.env.PORT || 8888
-const hostname = process.env.HOST_NAME
+const app = express();
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 //console.log("check log : ", process.env)
 
@@ -15,6 +16,14 @@ configViewEngine(app);
 
 //route
 app.use('/v1' ,webRouters);
+
+// simple query
+connection.query(
+    'SELECT * FROM Users u',
+    function(err, results, fields) {
+      console.log(">>>results= ", results); // results contains rows returned by server
+    }
+);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
