9c5f82704f4eacacd2f49cade5e108422b7e077f
TrungNQ <trungnq15992@gmail.com>
2022-12-26 17:31:55 +0700

    API Create User

--- src/controllers/apiControllers.js
+++ src/controllers/apiControllers.js
@@ -8,6 +8,22 @@ const getUsersAPI = async (req, res) => {
     })
 }
 
+const postCreateUserAPI = async (req, res) => {
+    let email = req.body.emailId;
+    let name = req.body.myname;
+    let city = req.body.city;
+
+    let user = await User.create({
+        email: email,
+        name: name,
+        city: city
+    })
+    return res.status(200).json({
+        errorCode: 0,
+        data: user
+    })
+}
+
 module.exports = {
-    getUsersAPI
+    getUsersAPI, postCreateUserAPI
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const routerAPI = express.Router();
 
-const {getUsersAPI} = require('../controllers/apiControllers');
+const {getUsersAPI, postCreateUserAPI} = require('../controllers/apiControllers');
 
 routerAPI.get('/', (req,res) => {
     res.send("Hello World")
 });
 
 routerAPI.get('/users', getUsersAPI);
+routerAPI.post('/users', postCreateUserAPI);
 
 module.exports = routerAPI;
