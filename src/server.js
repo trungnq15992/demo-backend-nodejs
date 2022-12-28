d133beaebffba18ac345426902d4596226b70a26
TrungNQ <trungnq15992@gmail.com>
2022-12-28 17:16:01 +0700

    Connect

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const apiRouters = require('./routes/api');
 const connection = require('./config/database');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -32,7 +33,19 @@ app.use('/v1/api/', apiRouters);
 //test connection
 (async () => {
     try {
-        await connection();
+        //using mongooose
+        //await connection();
+
+        //using mongodb driver
+        const url = process.env.DB_HOST_WITH_DRIVER;
+        const client = new MongoClient(url);
+        const dbName = process.env.DB_NAME;
+
+        await client.connect();
+        console.log('Connected successfully to server');
+        const db = client.db(dbName);
+        const collection = db.collection('documents');
+
         app.listen(port, () => {
             console.log(`Backend_NodeJS app listening on port ${port}`)
         })
